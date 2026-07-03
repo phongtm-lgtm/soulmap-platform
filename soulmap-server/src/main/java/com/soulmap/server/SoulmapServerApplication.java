354e41d685e7c93cdb7d2836c59891df808969f3
manhphongdev <tranmanhphong2002@gmail.com>
2026-07-03 19:26:04 +0700

    config dotenv

---
+++ soulmap-server/src/main/java/com/soulmap/server/SoulmapServerApplication.java
@@ -0,0 +1,13 @@
+package com.soulmap.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoulmapServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoulmapServerApplication.class, args);
+	}
+
+}
